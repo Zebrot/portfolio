13071f7174d94c779f7b3af004b29515708e3e94
Pierre Terrancle <pi.terrancle@gmail.com>
2025-10-02 17:16:52 +0200

    update for deployment

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return(
     <div className="container mx-auto max-w-3xl py-8 flex flex-col gap-10 rounded-lg">      
       <span className="text-[224px] tracking-[15px] text-center">404</span>
-      <Link href='/' className="text-center text-[24px] hover:underline">on dirait que vous êtes perdu... cliquez ici pour retrouver votre chemin</Link>
+      <Link href='/' className="text-center text-[24px] hover:underline">on dirait que vous êtes perdu... Cliquez ici pour retrouver votre chemin</Link>
     </div>
 
   )
